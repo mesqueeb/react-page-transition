44a99c6a4503a500e2fe1b658add9c601325d7b6
Luca Ban <mesqueeb@users.noreply.github.com>
2025-06-05 09:24:03 +1200

    feat: add demo-reach-router

---
+++ demo-reach-router/src/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+// import { BrowserRouter, Link, Route, Switch } from 'react-router-dom'
+import './styles.css'
+
+// const Home = () => <h1>Home</h1>
+
+// const About = () => <h1>About</h1>
+
+function App() {
+  return (
+    <React.StrictMode>
+      OK
+      {/* <BrowserRouter>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Route
+          render={({ location }) => {
+            return (
+              <PageTransition preset="moveToLeftFromRight" transitionKey={location.pathname}>
+                <Switch location={location}>
+                  <Route exact path="/" component={Home} />
+                  <Route exact path="/about" component={About} />
+                </Switch>
+              </PageTransition>
+            )
+          }}
+        />
+      </BrowserRouter> */}
+    </React.StrictMode>
+  )
+}
+
+export default App
