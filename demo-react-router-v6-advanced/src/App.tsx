13cc2f60eec9827d48d0da1aedd921f8056b002c
Luca Ban <mesqueeb@users.noreply.github.com>
2025-06-05 11:27:00 +1200

    feat: implement advanced preview

---
+++ demo-react-router-v6-advanced/src/App.tsx
@@ -0,0 +1,82 @@
+import { PageTransition, presets } from '@mesqueeb/react-page-transition'
+import React, { useState } from 'react'
+import { BrowserRouter, Link, Route, Routes, useLocation } from 'react-router-dom'
+import { pages } from './pages'
+import './styles.css'
+
+function RoutesWrapper({ preset }: { preset: string }) {
+  const location = useLocation()
+  return (
+    <PageTransition
+      preset={preset}
+      transitionKey={location?.pathname}
+      style={{ flex: 1, display: 'flex', flexDirection: 'column', alignItems: 'stretch' }}
+      contentStyle={{ flex: 1, display: 'flex', flexDirection: 'column', alignItems: 'stretch' }}
+    >
+      <Routes location={location}>
+        {pages.map((page) => (
+          <Route
+            key={page.path}
+            path={page.path}
+            element={
+              <div style={{ flex: 1, background: page.color, padding: '16px', color: 'white' }}>
+                <h1>{page.title}</h1>
+              </div>
+            }
+          />
+        ))}
+      </Routes>
+    </PageTransition>
+  )
+}
+
+function PageControls({
+  preset,
+  selectNextPreset,
+  setPreset,
+}: {
+  preset: string
+  selectNextPreset: () => void
+  setPreset: (preset: string) => void
+}) {
+  const location = useLocation()
+  const currentIndex = pages.findIndex((page) => page.path === location.pathname)
+
+  return (
+    <div style={{ display: 'flex', gap: '16px', padding: '16px' }}>
+      <Link to={pages[currentIndex + 1 === pages.length ? 0 : currentIndex + 1].path}>
+        Next Page
+      </Link>
+      <button onClick={selectNextPreset}>Next Preset</button>
+      <select value={preset} onChange={(e) => setPreset(e.target.value)}>
+        {Object.keys(presets).map((key) => (
+          <option key={key} value={key}>
+            {key}
+          </option>
+        ))}
+      </select>
+    </div>
+  )
+}
+
+function App() {
+  const [preset, setPreset] = useState('moveToLeftFromRight')
+  function selectNextPreset() {
+    const presetEntries = Object.entries(presets)
+    const currentPresetEntryIndex = presetEntries.findIndex(([key]) => key === preset)
+    const nextPresetEntry = presetEntries[currentPresetEntryIndex + 1]
+    if (nextPresetEntry) {
+      setPreset(nextPresetEntry[0])
+    }
+  }
+  return (
+    <React.StrictMode>
+      <BrowserRouter>
+        <PageControls preset={preset} setPreset={setPreset} selectNextPreset={selectNextPreset} />
+        <RoutesWrapper preset={preset} />
+      </BrowserRouter>
+    </React.StrictMode>
+  )
+}
+
+export default App
